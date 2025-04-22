a8abbf11fd68393b21f662ca1c5fc971e078308d
Edgar Simo-Serra <ess@waseda.jp>
2025-04-22 18:57:21 +0900

    Reworked Lua environments. I like them now.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,111 @@
+pub struct Lua {
+    lua: mlua::Lua,
+    globals: mlua::Table,
+    env_mt: mlua::Table,
+}
+impl Lua {
+    pub fn new() -> mlua::Result<Self> {
+        let lua = mlua::Lua::new();
+        lua.load_std_libs( mlua::StdLib::ALL_SAFE )?;
+
+        // Swap the global table with an empty one, returning the global one
+        let globals: mlua::Table = unsafe { lua.exec_raw( (), |state| {
+            mlua::ffi::lua_pushvalue( state, mlua::ffi::LUA_GLOBALSINDEX );
+            mlua::ffi::lua_newtable(state);
+            mlua::ffi::lua_replace( state, mlua::ffi::LUA_GLOBALSINDEX );
+        }) }?;
+
+        // Set globals to be truly read only (should only be possible if the user is being bad)
+        let globals_mt = lua.create_table()?;
+        globals_mt.set( "__newindex", lua.create_function( |_, ()| -> mlua::Result<()> {
+            Err(mlua::Error::RuntimeError(String::from("globals are read only")))
+        } )?)?;
+        globals.set_metatable( Some(globals_mt) );
+
+        // Our new globals should be usable now
+        let wrapped = lua.globals();
+        let wrapped_mt = lua.create_table()?;
+        wrapped_mt.set( "__index", lua.create_function( |lua, (t, k): (mlua::Table, mlua::Value)| -> mlua::Result<mlua::Value> {
+            let env_str = lua.create_string("_ENV")?;
+            match k == mlua::Value::String(env_str) {
+                true => t.raw_get( k ),
+                false => {
+                    let e: mlua::Table = t.raw_get("_ENV")?;
+                    e.get(k)
+                },
+            }
+        } )?)?;
+        wrapped_mt.set( "__newindex",lua.create_function( |_, (t, k, v): (mlua::Table, mlua::Value, mlua::Value)| -> mlua::Result<()> {
+            let e: mlua::Table = t.raw_get("_ENV")?;
+            e.set(k, v)
+        } )?)?;
+        wrapped.set_metatable( Some(wrapped_mt) );
+
+        let env_mt = lua.create_table()?;
+        env_mt.set( "__index", globals.clone() )?;
+
+        Ok(Lua{
+            lua,
+            globals,
+            env_mt,
+        })
+    }
+}
+
+pub struct Env {
+    table: mlua::Table,
+}
+impl Env {
+    pub fn new( lua: &Lua ) -> mlua::Result<Self> {
+        let table = lua.lua.create_table()?;
+        table.set_metatable( Some(lua.env_mt.clone()) );
+        Ok(Env{table})
+    }
+
+    pub fn set( &self, lua: &Lua ) -> mlua::Result<()> {
+        lua.lua.globals().raw_set( "_ENV", self.table.clone() )
+    }
+}
+
 fn main() -> mlua::Result<()> {
-   #[allow(non_snake_case)]
-   let L = unsafe{ mlua::ffi::luaL_newstate() };
-   let lua = unsafe { mlua::Lua::init_from_ptr( L ) };
+    let lua = Lua::new()?;
 
-   unsafe {
-      mlua::ffi::luaopen_base( L );
-   }
+    let env1 = Env::new( &lua )?;
+    let env2 = Env::new( &lua )?;
 
-   let t = lua.create_table()?;
+    env1.set( &lua )?;
+    lua.lua.load("
+    assert( cat == nil )
+    cat = 5
+    assert( cat == 5 )
+    cat = 7
+    assert( cat == 7 )
+    print('TEST1 OK')
+    ").exec()?;
 
-   let m = lua.create_table()?;
-   m.set("__index", lua.globals())?;
-   t.set_metatable(Some(m));
+    env2.set( &lua )?;
+    lua.lua.load("
+    assert( cat == nil )
+    cat = 9
+    assert( cat == 9 )
+    print('TEST2 OK')
+    ").exec()?;
 
-   t.set( "testfunc", lua.create_function( |lua, ()| {
-       let name: mlua::Value = lua.globals().get("__name")?;
-       dbg!(name);
-       Ok(())
-    })?)?;
+    env1.set( &lua )?;
+    lua.lua.load("
+    assert( cat == 7 )
+    print('TEST3 OK')
+    ").exec()?;
 
-   t.set("__name", "foo" )?;
-   let rk = lua.create_registry_value( t )?;
+    lua.globals.raw_set( "notouchie", 1 )?;
+    lua.lua.load("
+    assert( notouchie == 1 )
+    notouchie = 2
+    assert( notouchie == 2 )
+    notouchie = nil
+    assert( notouchie == 1 )
+    print('TEST4 OK')
+    ").exec()?;
 
-   lua.load("
-function main ( str )
-   print( __name, str )
-   testfunc()
-end
-   ").exec()?;
-
-   unsafe {
-      mlua::ffi::lua_getglobal( L, c"main".as_ptr() );
-      mlua::ffi::lua_pushstring( L, c"hello world".as_ptr() );
-      mlua::ffi::lua_rawgeti( L, mlua::ffi::LUA_REGISTRYINDEX, rk.id().into() );
-      mlua::ffi::lua_setfenv( L, -3 );
-      if mlua::ffi::lua_pcall( L, 1, 0, 0 ) != 0{
-          dbg!( std::ffi::CStr::from_ptr( mlua::ffi::luaL_tolstring( L, -1, std::ptr::null_mut() ) ) );
-      }
-   }
-
-   Ok(())
+    Ok(())
 }
